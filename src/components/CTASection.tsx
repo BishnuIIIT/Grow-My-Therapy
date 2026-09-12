77922f487aa7614a31c51d6fa36ee0b7ee9912e1
Bishnu <Bishnu@prasad.com>
2026-09-12 21:36:53 +0530

    style: increase right CTA width by 10% while preserving exact vertical height

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -58,9 +58,9 @@ export default function CTASection() {
           </Link>
         </div>
 
-        {/* Right Image (Height increased by ~15%) */}
-        <div className="relative w-[33vw] xl:w-[38vw] max-w-[600px] min-w-[260px] flex-shrink-0">
-          <div className="relative w-full aspect-square">
+        {/* Right Image (Width increased 10%, height maintained) */}
+        <div className="relative w-[36vw] xl:w-[42vw] max-w-[660px] min-w-[260px] flex-shrink-0">
+          <div className="relative w-full aspect-[11/10]">
             <Image
               src="/images/cta-right.jpg"
               alt="A therapy session in progress, showing a therapist taking notes while listening attentively to a client."
